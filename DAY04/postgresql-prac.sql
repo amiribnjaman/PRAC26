b54c59ced3f12eb53eafe36f8da9a8ffa9ddac78
amiribnjaman <amiribnjaman@gmail.com>
2026-01-11 18:47:51 +0600

    INSERT query prac)

--- DAY04/postgresql-prac.sql
+++ DAY04/postgresql-prac.sql
@@ -14,9 +14,22 @@ CREATE TABLE employees (
 -- Create a table for storing staff information
 CREATE TABLE staff (
     staff_id SERIAL PRIMARY KEY,
-    name VARCHAR(50),
+    full_name VARCHAR(50),
     email VARCHAR(50),
     department VARCHAR(50)
-)
+);
 
 
+-- Insert sample data into the employees table
+INSERT INTO 
+    employees (first_name, last_name, hire_date, salary)
+    VALUES 
+    ('John', 'Doe', '2020-01-15', 60000),
+    ('Jane', 'Smith', '2019-03-22', 75000),
+    ('Emily', 'Johnson', '2021-07-30', 50000);
+
+-- Insert sample data into the staff table
+INSERT INTO 
+    staff (full_name, email, department)
+    VALUES 
+    ('Alice Brown', 'alice.brown@example.com', 'HR');
